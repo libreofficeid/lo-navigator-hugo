7ae5b02f27cd66309f6237a7c3aeee07f17c528b
Sofyan Sugianto <sofyanartem@gmail.com>
2019-12-06 21:01:41 +0700

    remove maps in main script js

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,18 +26,7 @@
 		$(this).replaceWith(video);
 	});
 	
-	/* ========================================================================= */
-	/*	Portfolio Filtering Hook
-	/* =========================================================================  */
-	setTimeout(function () {
-		var filterizd = $('.filtr-container').filterizr({});
-		//Active changer
-		$('.filtr-control').on('click', function () {
-			$('.filtr-control').removeClass("active");
-			$(this).addClass("active");
-		});
-	}, 500);
-	
+
 	/* ========================================================================= */
 	/*	Testimonial Carousel
 	/* =========================================================================  */
@@ -213,78 +202,3 @@
 	
 	
 	})(jQuery);
-	
-	
-	
-	window.marker = null;
-	
-	function initialize() {
-	var map;
-	
-	var latitude = $('#map').data('lat');
-	var longitude = $('#map').data('long');
-	var nottingham = new google.maps.LatLng(latitude, longitude);
-	
-	var style = [{
-		"stylers": [{
-			"hue": "#ff61a6"
-		}, {
-			"visibility": "on"
-		}, {
-			"invert_lightness": true
-		}, {
-			"saturation": 40
-		}, {
-			"lightness": 10
-		}]
-	}];
-	
-	var mapOptions = {
-		// SET THE CENTER
-		center: nottingham,
-	
-		// SET THE MAP STYLE & ZOOM LEVEL
-		mapTypeId: google.maps.MapTypeId.ROADMAP,
-		zoom: 9,
-	
-		// SET THE BACKGROUND COLOUR
-		backgroundColor: "#000",
-	
-		// REMOVE ALL THE CONTROLS EXCEPT ZOOM
-		zoom: 17,
-		panControl: false,
-		zoomControl: true,
-		mapTypeControl: false,
-		scaleControl: false,
-		streetViewControl: false,
-		overviewMapControl: false,
-		zoomControlOptions: {
-			style: google.maps.ZoomControlStyle.LARGE
-		}
-	
-	}
-	map = new google.maps.Map(document.getElementById('map'), mapOptions);
-	
-	// SET THE MAP TYPE
-	var mapType = new google.maps.StyledMapType(style, {
-		name: "Grayscale"
-	});
-	map.mapTypes.set('grey', mapType);
-	map.setMapTypeId('grey');
-	
-	//CREATE A CUSTOM PIN ICON
-	var marker_image = $('#map').data('marker');
-	var pinIcon = new google.maps.MarkerImage(marker_image, null, null, null, new google.maps.Size(25, 33));
-	
-	marker = new google.maps.Marker({
-		position: nottingham,
-		map: map,
-		icon: pinIcon,
-		title: 'navigator'
-	});
-	}
-	
-	var map = $('#map');
-	if (map.length != 0) {
-	google.maps.event.addDomListener(window, 'load', initialize);
-	}
